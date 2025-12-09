85f4605a822806abe03873d2b7db2da11883a11d
baqar-abbas <baqar.abbas@hotmail.com>
2025-12-10 00:29:46 +0500

    Setup Redux toolkit store and hooks for Redux setup

---
+++ src/app/store.ts
@@ -0,0 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import tasksReducer from "../features/tasks/tasksSlice";
+import socketReducer from "../features/socket/socketSlice";
+// import { authApi } from '../features/auth/authApi';
+// import { tasksApi } from '../features/tasks/tasksApi';
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    tasks: tasksReducer,
+    socket: socketReducer,
+    // [authApi.reducerPath]: authApi.reducer,
+    // [tasksApi.reducerPath]: tasksApi.reducer,
+  },
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware()
+  //     .concat(authApi.middleware)
+  //     .concat(tasksApi.middleware),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
